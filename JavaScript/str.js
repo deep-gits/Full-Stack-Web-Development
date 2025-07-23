bd5931b259f03ca158023c2d7eeec62c62a0f316
Deepak saraswat <deepaksaraswat8913@gmail.com>
2025-07-23 22:31:27 +0530

    Add files via upload

---
+++ JavaScript/str.js
@@ -0,0 +1,9 @@
+// let password= prompt("enter");
+// let newpass=password.trim();
+// console.log(newpass);
+let msg="     ilovecoding         ";
+newsmsg= msg.trim().toUpperCase();
+console.log(newsmsg)
+console.log(newsmsg.slice(4,msg.length))
+let a= msg.replace("love","hate");
+console.log(a)
