0e46508b255fd145347fa17fe5cb91154ad49671
Deepak saraswat <deepaksaraswat8913@gmail.com>
2025-07-23 21:06:11 +0530

    Add files via upload

---
+++ JavaScript/app.js
@@ -0,0 +1,27 @@
+// console.log("heeloooo")
+// console.log("why")
+let a=12;
+let b=12;
+console.log(a+b)
+
+let pencilprice =13;
+let erasorprice =12;
+console.log("the total is :",pencilprice+erasorprice)
+console.log(`thye total price is ${pencilprice+erasorprice}`)
+c='a' <'b';
+console.log(c)
+age = 12;
+if(age<=13){
+    console.log("tfytf")
+}
+else{
+    console.log("Sssa")
+}
+
+// alert("myname is deepka")
+// console.error("this is an error")
+// console.warn("warn")
+
+let first = prompt("enter your name")
+let last = prompt("enter your name")
+alert(first," ",last)
