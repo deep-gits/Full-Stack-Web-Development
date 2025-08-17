b06a4e9c1b2e79feecd616fa879acd3ac0bb83c3
Deepak saraswat <deepaksaraswat8913@gmail.com>
2025-08-17 21:08:41 +0530

    Add files via upload

---
+++ JavaScript/settimeout.js
@@ -0,0 +1,20 @@
+console.log("hi there");
+
+setTimeout( ()=>{
+    console.log("apna collage")
+},4000)
+
+console.log("welcome to")
+
+let id =setInterval( ()=>{
+    console.log("apna collage")
+},2000)
+
+console.log(id);
+
+let id2 =setInterval( ()=>{
+    console.log("apna collage")
+},2000)
+
+console.log(id2);
+
